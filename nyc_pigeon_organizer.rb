fe75ee9f8b32e22ddbfa36413f67115a84117955
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-15 18:12:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,5 @@
 def nyc_pigeon_organizer(array)
   final = {}
-  array.each do |first_level, all_other|
-    all_other.each do |category, type|
-      type.each do |name|
-        final[name] = {:color => [], :gender => [], :lives => []}
-      end
-    end 
-  end 
+  array.each do |k, v|
+    
 end 
