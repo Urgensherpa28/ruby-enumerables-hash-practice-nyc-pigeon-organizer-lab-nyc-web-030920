ff878bc0a85d4bf4ccc2e2474d6176e55b4b8c8f
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-15 18:34:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(array)
     name.each do |bird_name|
       x.each do |item|
         if bird_name == item 
-          final[item][:color] << color.to_s 
+          final[item][:color] << bird_color.to_s 
         end
       end
     end
